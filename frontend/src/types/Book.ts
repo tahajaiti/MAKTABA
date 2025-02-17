5d84451d39c4bfb311aee1ea69652b9deb223058
tahajaiti <kyojin.rar@gmail.com>
2025-02-17 17:05:12 +0100

    FEAT: added the main interfaces for book and user

---
+++ frontend/src/types/Book.ts
@@ -0,0 +1,11 @@
+interface Book {
+    id: number,
+    title: string,
+    author: string,
+    cover: string,
+    quantity: number,
+    created_at: Date,
+    updated_at: Date
+}
+
+export default Book;
---
+++ frontend/src/types/User.ts
@@ -0,0 +1,10 @@
+interface User {
+    id: number,
+    name: string,
+    email: string,
+    role_id: number,
+    created_at: Date,
+    updated_at: Date
+}
+
+export default User;
