26a243141359fc34ea447ede808a145544143010
tahajaiti <kyojin.rar@gmail.com>
2025-02-18 09:15:40 +0100

    FEAT: added a helper for standardizing my api responses

---
+++ backend/app/Helpers/ApiResponse.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Helpers;
+
+class ApiResponse
+{
+
+    public static function success($data, $message = 'success', $code = 200): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data,
+        ]);
+    }
+
+    public static function error($message, $code = 400): \Illuminate\Http\JsonResponse {
+        return response()->json([
+            'status' => 'error',
+            'message' => $message,
+            'data' => null,
+        ]);
+    }
+
+
+}
