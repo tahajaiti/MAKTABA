a0d4a543d8aaee9bcd9edc35e991a1ed361efc4e
tahajaiti <kyojin.rar@gmail.com>
2025-02-19 15:28:58 +0100

    FEAT: created the book service to handle api crud calls with the backend

---
+++ frontend/src/services/bookService.ts
@@ -0,0 +1,20 @@
+import api from '../api/apiClient';
+import Book from '../types/Book';
+import Response from '../types/Response';
+
+
+const getAll = () => api.get<Response<Book[]>>('/books');
+const get = (id: number) => api.get<Response<Book>>(`/books/${id}`);
+const create = (data: Book) => api.post<Response<Book>>('/books', data);
+const update = (id: number, data: Omit<Book, 'id'>) => api.put<Response<Book>>(`/books/${id}`, data);
+const remove = (id: number) => api.delete<Response<null>>(`/books/${id}`);
+
+const bookService = {
+    getAll,
+    get,
+    create,
+    update,
+    remove
+}
+
+export default bookService;
